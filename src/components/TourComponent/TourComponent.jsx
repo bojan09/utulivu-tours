b3cfce06341c54b9958b94e7e540beec3c406547
bojan09 <bojan-stanimirovski097@hotmail.com>
2024-06-10 16:35:52 +0200

    Tour Component, Desktop Responsive

--- src/components/TourComponent/TourComponent.jsx
+++ src/components/TourComponent/TourComponent.jsx
@@ -20,28 +20,30 @@ const TourComponent = ({ tours }) => {
   return (
     <div className="tour-container">
       <Carousel
-        arrows={false}
+        arrows={true}
         swipeable={true}
         draggable={false}
         showDots={false}
         infinite={true}
-        autoPlay={true}
-        autoPlaySpeed={8000}
         customTransition="transform 600ms ease-in-out"
         transitionDuration={300}
         responsive={responsive}
+        className="carousel"
       >
-        <img src={tours.img_1} alt={tours.img_1} />
-        <img src={tours.img_2} alt={tours.img_2} />
-        <img src={tours.img_3} alt={tours.img_3} />
+        <img className="tour-image" src={tours.img_1} alt={tours.img_1} />
+        <img className="tour-image" src={tours.img_2} alt={tours.img_2} />
+        <img className="tour-image" src={tours.img_3} alt={tours.img_3} />
       </Carousel>
-      <h1 className="tour-heading">{tours.title}</h1>
-      <p className="tour-description">This tour offers the following:</p>
-      <div className="tour-offers">
-        <p className="tour-blog">{tours.tour_offer_1}</p>
-        <p className="tour-blog">{tours.tour_offer_2}</p>
-        <p className="tour-blog">{tours.tour_offer_3}</p>
-        <p className="tour-blog">{tours.tour_offer_4}</p>
+
+      <div className="tour-info_container">
+        <h1 className="tour-heading">{tours.title}</h1>
+        <p className="tour-description">This tour offers the following:</p>
+        <div className="tour-offers">
+          <p className="tour-blog">{tours.tour_offer_1}</p>
+          <p className="tour-blog">{tours.tour_offer_2}</p>
+          <p className="tour-blog">{tours.tour_offer_3}</p>
+          <p className="tour-blog">{tours.tour_offer_4}</p>
+        </div>
       </div>
     </div>
   );
